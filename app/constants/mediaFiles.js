635baf65a1f8f6a72ce0f1a42e30cb0434393b86
stellamaa <stellaam@live.com>
2026-03-25 09:51:14 +0000

    push

--- app/constants/mediaFiles.js
+++ app/constants/mediaFiles.js
@@ -3,10 +3,10 @@ import { getMediaUrl } from '../utils/getMediaUrl';
 export const mediaFiles = [
   { src: getMediaUrl('/assets/AshleySaville.mov'), type: 'video', url: 'https://ashleysaville.com' , title: 'Design and code for art gallery Ashley Saville, made with Next.js, Sanity and ArtLogic API'},
   { src: getMediaUrl('/assets/rielavideo.mp4'), type: 'video', url: 'https://rielaspaces.com' , title: 'Website for Riela Bathhouse, made with WEBGL and JS'},
-  { src: getMediaUrl('/assets/fionavideo.mp4'), type: 'video', url: 'https://fionaalbrow.com' , title: 'Fiona Albrow portfolio site made with Squarespace' },
   { src: getMediaUrl('/assets/jeromevideo.mp4'), type: 'video', url: 'https://jeromemusic.co.uk/', title: 'Design and development of website for artist Jerome, made with React'},
-  { src: getMediaUrl('/assets/WellVideo.mov'), type: 'video', url: 'https://www.valmottagning.se/', title: 'UX-research and prototyping for pshycology clinic Well'},
   { src: getMediaUrl('/assets/nynspacevideo.mp4'), type: 'video', url: 'https://nynspace.com/', title: 'Design and development for meditation and art organisation NYN Space, made with React and Sanity'},
+  { src: getMediaUrl('/assets/fionavideo.mp4'), type: 'video', url: 'https://fionaalbrow.com' , title: 'Fiona Albrow portfolio site made with Squarespace' },
+  { src: getMediaUrl('/assets/WellVideo.mov'), type: 'video', url: 'https://www.valmottagning.se/', title: 'UX-research and prototyping for pshycology clinic Well'},
   { src: getMediaUrl('/assets/stellaoldvideo.mp4'), type: 'video' , title:' Old personal site, made with HTML, CSS, and JS'},
   { src: getMediaUrl('/assets/image1.png'), type: 'image', title: 'Touch in Vein - P5.js experiment'},
   { src: '/assets/img3.png', type: 'image' , title: 'Digital Hand, generative art sketch made with p5.js' },
