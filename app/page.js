15139f4436af156338524bc9c10ffc21e60ce821
stellamaa <stellaam@live.com>
2026-06-04 08:03:25 +0100

    push

--- app/page.js
+++ app/page.js
@@ -23,7 +23,7 @@ export default function HomePage() {
      
       <div className="site-bio">
         <span>Stella Mathioudakis</span>
-        <span> design focused frontend developer / sound artist</span>
+        <span> design focused developer / sound artist</span>
         <span>working across web, code, and sound</span>
         <span>exploring how technology can feel intentional.</span>
         <span>building digital spaces that invite reflection and</span>
